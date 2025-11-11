90bbadfd1ebdcabe4863529a870fa90f83ce620d
Ashraf <ashrafmohiuddin666@gmail.com>
2025-11-11 10:42:30 +0530

    Update AWS provider version to 6.20.0

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "6.20.0"
     }
   }
 }
-
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
